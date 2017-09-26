dd6a33dfd67599eba15d02035d3800af72cff93b
Cristina Fernández <mcferrod@gmail.com>
2017-09-26 18:49:48 +0200

    add imgs pages to slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,7 +6,14 @@
 
   'use strict';
   $(document).ready(function() {
-    $('.single-item').slick();
+    $('.single-item').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
+    });
   });
 
   $(function() {
